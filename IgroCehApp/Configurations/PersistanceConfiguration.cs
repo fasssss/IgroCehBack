b4e4ddb6e94a6c7bef9bfad1bf39ba83b7766a64
Andrei.Buinski <buinsky.andrei@gmail.com>
2024-10-31 14:06:36 +0300

    MySQL connection options was modified

--- IgroCehApp/Configurations/PersistanceConfiguration.cs
+++ IgroCehApp/Configurations/PersistanceConfiguration.cs
@@ -11,7 +11,11 @@ namespace API.Configurations
         {
             var defaultConnectionString = configuration.GetConnectionString("Default");
             var mySqlVersion = configuration.GetValue<string>("MySQLVersion") ?? "8.0.27";
-            services.AddMySql<IgroCehContext>(defaultConnectionString, new MySqlServerVersion(new Version(mySqlVersion)));
+            services.AddMySql<IgroCehContext>(defaultConnectionString, new MySqlServerVersion(new Version(mySqlVersion)), 
+                options => options.EnableRetryOnFailure(
+                    maxRetryCount: 5,
+                    maxRetryDelay: System.TimeSpan.FromSeconds(30),
+                    errorNumbersToAdd: null));
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IGuildRepository, GuildRepository>();
             services.AddScoped<IEventRepository, EventRepository>();
