d33341d1f25fa43d33a8e6ddc618b984665e1048
Andrei.Buinski <buinsky.andrei@gmail.com>
2024-12-07 06:45:18 +0300

    File upload was added

---
+++ IgroCehApp/Endpoints/Post/SuggestGame.cs
@@ -0,0 +1,34 @@
+﻿using API.Helpers;
+using API.RepR.Request;
+using API.RepR.Response;
+using Application.ApplicationInterfaces;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Security.Claims;
+
+namespace API.Endpoints.Post
+{
+    public class SuggestGame: Endpoint<SuggestGameRequest, Results<Ok, BadRequest<string>>>
+    {
+        private readonly IEventApplicationService _eventApplicationService;
+        private readonly WebSocketHelper _webSocketHelper;
+
+        public SuggestGame(IEventApplicationService eventApplicationService, WebSocketHelper webSocketHelper)
+        {
+            _eventApplicationService = eventApplicationService;
+            _webSocketHelper = webSocketHelper;
+        }
+
+        public override void Configure()
+        {
+            Post("/api/suggestGame");
+            AllowFileUploads();
+        }
+
+        public override async Task<Results<Ok, BadRequest<string>>> ExecuteAsync(SuggestGameRequest request, CancellationToken ct)
+        {
+            var stringId = HttpContext.User.Claims.Single(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            return TypedResults.Ok();
+        }
+    }
+}
---
+++ IgroCehApp/RepR/Request/SuggestGameRequest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.RepR.Request
+{
+    public class SuggestGameRequest
+    {
+        public string EventRecordId { get; set; }
+        public string GameName { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
