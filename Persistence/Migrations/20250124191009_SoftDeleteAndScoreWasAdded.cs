078ed7900aa19d332ac458f20f541ba7f6ee64e3
Andrei.Buinski <buinsky.andrei@gmail.com>
2025-01-24 22:20:55 +0300

    migrations were added

---
+++ Persistence/Migrations/20250124191009_SoftDeleteAndScoreWasAdded.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SoftDeleteAndScoreWasAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "UserGuilds",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Score",
+                table: "UserGuilds",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "UserGuilds");
+
+            migrationBuilder.DropColumn(
+                name: "Score",
+                table: "UserGuilds");
+        }
+    }
+}
